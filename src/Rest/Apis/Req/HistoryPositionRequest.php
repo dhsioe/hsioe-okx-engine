d029f9d9756f2b1178103c2e2be22073ade64b20
hsioe <dawei0937@gmail.com>
2025-01-20 18:14:11 +0800

    feat: 完善获取持仓历史

--- src/Rest/Apis/Req/HistoryPositionRequest.php
+++ src/Rest/Apis/Req/HistoryPositionRequest.php
@@ -11,5 +11,59 @@ namespace Hsioe\QuantOkxApi\Rest\Apis\Req;
 
 class HistoryPositionRequest
 {
-    use RequestBase;
+    use RequestBase,ListTrait;
+    
+    
+    protected string $instId = '';
+    protected string $instType = 'SWAP';
+    
+    protected string $type = '2';
+    
+    /**
+     * @param string $instId
+     */
+    public function setInstId(string $instId): void
+    {
+        $this->instId = $instId;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getInstId(): string
+    {
+        return $this->instId;
+    }
+    
+    /**
+     * @param string $instType
+     */
+    public function setInstType(string $instType): void
+    {
+        $this->instType = $instType;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getInstType(): string
+    {
+        return $this->instType;
+    }
+    
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
 }
